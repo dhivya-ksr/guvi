cb21b7edb2ea94972f028433d38e1b4c4a4ddecf
dhivya-ksr <dhivyramesh@gmail.com>
2017-06-06 18:13:37 +0530

    Create conso1.java

---
+++ conso1.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class conso1
+{
+    public static void main(String args[])
+{
+    Scanner sc=new Scanner(System.in);
+    String s=sc.nextLine();
+    for(int i=0;i<5;i++)
+    {
+    System.out.println(s);
+    }
+}
+}
+

ce5a28beb1b010abb4483520a39ab9989dd07f86
dhivya-ksr <dhivyramesh@gmail.com>
2017-06-08 18:00:45 +0530

    Create Pack3.java

---
+++ Pack3.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+class Pack3
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+int b,sum=0,c;
+c=a;
+while(a!=0)
+{
+b=a%10;
+sum=sum*10+b;
+a=a/10;
+}
+if(c==sum)
+{
+System.out.println("Palindrome");
+}
+else
+{
+System.out.println("Not Palindrome");
+}
+}}
